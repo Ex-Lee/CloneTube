420a54a318849960010842e801c0298627b58dca
SangYeobLee <syg0519@gmail.com>
2021-03-22 16:11:36 +0900

    pug Template, local variables middleware 추가.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,12 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
+export const join = (req, res) => res.render("join", { pageName: "Join" });
+export const login = (req, res) => res.render("login", { pageName: "Login" });
+export const logout = (req, res) =>
+  res.render("logout", { pageName: "Logout" });
 
-export const user = (req, res) => res.send("user");
-export const userDetail = (req, res) => res.send("User Detail");
-export const editProfile = (req, res) => res.send("Edit Profile");
-export const changePassword = (req, res) => res.send("Change Password");
+export const user = (req, res) => res.render("user", { pageName: "User" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageName: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageName: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageName: "Change Password" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,13 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
+export const home = (req, res) => res.render("home", { pageName: "Home" });
+export const search = (req, res) =>
+  res.render("search", { pageName: "Search" });
 
-export const video = (req, res) => res.send("Video");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Vidoe");
+export const video = (req, res) => res.render("video", { pageName: "Video" });
+export const upload = (req, res) =>
+  res.render("upload", { pageName: "Upload" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageName: "Video Detail" });
+export const editVideo = (req, res) =>
+  res.redner("editVideo", { pageName: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.redner("deleteVideo", { pageName: "Delete Video" });
