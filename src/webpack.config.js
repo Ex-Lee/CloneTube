db906b4348191b330f29984b8e031d4bd946c53a
SangYeobLee <syg0519@gmail.com>
2021-04-22 02:55:31 +0900

    webpack.config.js 수정 for heroku

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,42 +1,45 @@
 const path = require("path");
 const autoprefixer = require("autoprefixer");
-const ExtractCSS = require("extract-text-webpack-plugin");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ["@babel/polyfill", ENTRY_FILE],
+  entry: ENTRY_FILE,
   mode: MODE,
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(scss)$/,
         use: [
           {
-            loader: "babel-loader",
+            loader: MiniCssExtractPlugin.loader,
           },
-        ],
-      },
-      {
-        test: /\.(scss)$/,
-        use: ExtractCSS.extract([
           {
             loader: "css-loader",
           },
           {
             loader: "postcss-loader",
             options: {
-              plugins() {
-                return [autoprefixer({ browsers: "cover 99.5%" })];
+              postcssOptions: {
+                plugins: [
+                  [
+                    autoprefixer,
+                    {
+                      browsers: "cover 99.5%",
+                    },
+                  ],
+                ],
               },
             },
           },
           {
             loader: "sass-loader",
           },
-        ]),
+        ],
       },
     ],
   },
@@ -44,7 +47,13 @@ const config = {
     path: OUTPUT_DIR,
     filename: "[name].js",
   },
-  plugins: [new ExtractCSS("styles.css")],
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+    }),
+  ],
 };
 
 module.exports = config;
